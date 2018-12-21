d96d5018610b25fe17920eeb1c67e735348558b1
wgx <1551765394@qq.com>
2018-12-22 02:46:30 +0800

    武皮皮测试

---
+++ common/src/main/java/com/jk/model/Wgx.java
@@ -0,0 +1,14 @@
+package com.jk.model;
+
+public class Wgx {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
