53f13311cbc5b1ea8dbd9d02b6b0ff3e9052af03
cs <930743588@qq.com>
2019-01-04 10:26:45 +0800

    api

--- serviceapi/src/main/java/com/jk/serviceplaapi/TzeServiceapi.java
+++ serviceapi/src/main/java/com/jk/serviceplaapi/TzeServiceapi.java
@@ -1,14 +1,26 @@
 package com.jk.serviceplaapi;
 
-import com.jk.model.Order;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.jk.model.*;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RequestMapping("TzeServiceapi")
 public interface TzeServiceapi{
+
     @GetMapping("findList")
     List<Order> findList();
 
+    // 递归树
+    @GetMapping("findNva")
+    List<Nav> findNva();
+
+   @PostMapping("saveFigure")
+    void saveFigure(@RequestBody  Figure figures);
+
+//   @PostMapping("savetpf")
+//    void savetpf(@RequestParam("titleBeans") TitleBean titleBeans,@RequestParam("productBeans") ProductBean productBeans,@RequestParam("figures") Figure figures);
+//  @PostMapping("savetpf")
+//   void savetpf(Map<String, Object> map);
 }
